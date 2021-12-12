ba534d7f6b09c778e377186a999e36e34e2c554c
naveeak <50047632+naveeak@users.noreply.github.com>
2021-12-12 13:15:01 +0530

    Update vpn_docker_init.sh
    added more comments

--- vpn_docker_init.sh
+++ vpn_docker_init.sh
@@ -5,6 +5,8 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install docker.io
 
 #container based vpn once can start an ope vpn on a aws ec2 insatnce will one script .@credits https://github.com/kylemanna/docker-openvpn look there more details
+#this uses a docker to run the vpn server .once image create we can create a .ovpn config file for direct connection .For Android ,use the openvpn app and connect to the vpn server 
+#using the file.
 OVPN_DATA="ovpn-data"
 sudo docker volume create --name $OVPN_DATA
 sudo docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://$(ec2metadata --public-hostname)
